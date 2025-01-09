1814e6e8297324ecebcd1bf06d8f074a3b660135
ChoiYH96 <169019916+ChoiYH96@users.noreply.github.com>
2025-01-09 18:32:01 +0900

    [level 3] Title: 순위, Time: 1930.78 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,29 +1,20 @@
 def solution(n, results):
-    # 승패 관계를 담을 2D 배열 초기화
-    wins = [[0] * n for _ in range(n)]
+    match = {x:[[],[]] for x in range(1,n+1)}
+    answer = [ 0 for _ in range(n)]
     
-    # 경기 결과에 따라 wins 배열을 초기화
-    for a, b in results:
-        wins[a-1][b-1] = 1  # a가 b를 이김
-        wins[b-1][a-1] = -1 # b가 a에게 짐
-    
-    # 플로이드 와샬 알고리즘 적용 (모든 쌍 최단 경로, 여기서는 승패 관계)
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                if wins[i][k] == 1 and wins[k][j] == 1:  # i -> k, k -> j
-                    wins[i][j] = 1  # i가 j를 이김
-                if wins[i][k] == -1 and wins[k][j] == -1:  # i -> k, k -> j
-                    wins[i][j] = -1  # i가 j에게 짐
-    
-    # 정확한 순위를 매길 수 있는 선수 수 계산
-    answer = 0
-    for i in range(n):
-        count = 0
-        for j in range(n):
-            if wins[i][j] != 0:  # i와 j의 승패 관계가 확실
-                count += 1
-        if count == n - 1:  # 다른 모든 선수와 승패 관계가 확실하면
-            answer += 1
-    
-    return answer
+    for i in results:
+        match[i[0]][0] += [i[1]]
+        match[i[1]][1] += [i[0]]
+        
+    for k in range(1,n+1):
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if k == i or k == j or i == j:
+                    continue
+                else:
+                    if k in match[i][0] and j in match[k][0]:
+                        match[i][0] = list(set(match[i][0]+[j]))
+                    elif k in match[i][1] and j in match[k][1]:
+                        match[i][1] = list(set(match[i][1]+[j]))
+
+    return [len(x[0])+len(x[1]) for x in match.values()].count(n-1)
