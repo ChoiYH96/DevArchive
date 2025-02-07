a469a298d1077749b086e120b4aaac9964abfe54
ChoiYH96 <169019916+ChoiYH96@users.noreply.github.com>
2025-02-07 18:45:43 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,12 @@
+def solution(n):
+    
+    answer = 0
+    
+    while n > 0:
+        if n%2 != 0:
+            n -= 1
+            answer += 1
+        else:
+            n /= 2
+            
+    return answer
