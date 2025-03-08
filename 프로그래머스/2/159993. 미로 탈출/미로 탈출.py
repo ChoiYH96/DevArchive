599cfada0197854dfa3a759765a2f534f54748d0
ChoiYH96 <169019916+ChoiYH96@users.noreply.github.com>
2025-03-08 22:41:27 +0900

    [level 2] Title: 미로 탈출, Time: 28.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,70 +1,33 @@
-def route_start_lever(maps):
-    cnt = 1
-    temp = []
+def route(start, end, maps):
+    temp = [start]
     news = [[-1,0],[1,0],[0,-1],[0,1]]
-    maps = [[0 if y == "X" else y if y in ["S","L"] else 1 for y in x] for x in maps]
+    maps = [[0 if maps[x][y] == "X" or [x,y] == start else 1 for y in range(len(maps[0]))] for x in range(len(maps))]
     visited = [x[:] for x in maps]
-        
-    for i in range(len(maps)):
-        for j in range(len(maps[0])):
-            if maps[i][j] == "S":
-                temp = [[i,j]]
-        if len(temp) != 0:
-            break
-
+    
     while True:
         if len(temp) == 0:
-            break
+            return -1
         for i in temp[:]:
             for j in news:
                 idx = [i[0]+j[0],i[1]+j[1]]
                 if 0<=idx[0]<len(maps) and 0<=idx[1]<len(maps[0]):
-                    if maps[idx[0]][idx[1]] == "L":
-                        return cnt
+                    if idx == end:
+                        return maps[i[0]][i[1]]+1
                     elif visited[idx[0]][idx[1]] != 0 and maps[idx[0]][idx[1]] == 1:
-                        maps[idx[0]][idx[1]] = cnt
+                        maps[idx[0]][idx[1]] = maps[i[0]][i[1]]+1
                         temp = temp + [idx]
                         visited[idx[0]][idx[1]] = 0
             temp = temp[1:]
-        cnt+=1
-        
-    return -1
-
-def route_lever_exit(maps):
-    cnt = 1
-    temp = []
-    news = [[-1,0],[1,0],[0,-1],[0,1]]
-    maps = [[0 if y == "X" else y if y in ["E","L"] else 1 for y in x] for x in maps]
-    visited = [x[:] for x in maps]
-        
-    for i in range(len(maps)):
-        for j in range(len(maps[0])):
-            if maps[i][j] == "L":
-                temp = [[i,j]]
-        if len(temp) != 0:
-            break
-
-    while True:
-        if len(temp) == 0:
-            break
-        for i in temp[:]:
-            for j in news:
-                idx = [i[0]+j[0],i[1]+j[1]]
-                if 0<=idx[0]<len(maps) and 0<=idx[1]<len(maps[0]):
-                    if maps[idx[0]][idx[1]] == "E":
-                        return cnt
-                    elif visited[idx[0]][idx[1]] != 0 and maps[idx[0]][idx[1]] == 1:
-                        maps[idx[0]][idx[1]] = cnt
-                        temp = temp + [idx]
-                        visited[idx[0]][idx[1]] = 0
-            temp = temp[1:]
-        cnt+=1
-        
-    return -1
 
 def solution(maps):
-    answer = [route_start_lever(maps), route_lever_exit(maps)]
-    return sum(answer) if -1 not in answer else -1
-            
+    idx = dict()
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
+            if maps[i][j] in ["S","E","L"]:
+                idx[maps[i][j]] = [i,j]
+        if len(idx) == 3:
+            break
     
+    answer = [route(idx["S"],idx["L"],maps),route(idx["L"],idx["E"],maps)]
     
+    return -1 if -1 in answer else sum(answer) 
