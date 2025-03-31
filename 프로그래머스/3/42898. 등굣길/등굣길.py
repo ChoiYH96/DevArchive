2a8074677e3033f3efd4a3a8d97fe24a146c1b20
ChoiYH96 <169019916+ChoiYH96@users.noreply.github.com>
2025-03-31 15:03:26 +0900

    [level 3] Title: 등굣길, Time: 3.87 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -5,6 +5,6 @@ def solution(m, n, puddles):
     for i in range(1,n+1):
         for j in range(1,m+1):
             if [j,i] not in puddles:
-                maps[i][j] = (maps[i-1][j] + maps[i][j-1])%1000000007
+                maps[i][j] = (maps[i-1][j] + maps[i][j-1])
     
     return maps[n][m]%1000000007
